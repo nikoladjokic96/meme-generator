b30ccebccb8ef5ac95cd4767de2fffcbe51b8486
nikoladjokic96 <nikola0312996@gmail.com>
2022-11-02 18:56:40 +0100

    redeploy

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import trollface from '../components/trollface.png'
 
 export default function Nav() {
   return (
     <header className="nav">
       <div className="title">
-        <img className="logo--face" src="./trollface.png" />
+        <img className="logo--face" src={trollface} />
         <h2>MemeGenerator</h2>
       </div>
     </header>
